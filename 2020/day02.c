825b44d9920f00d0bf3adbb60b1c0492b68397b2
denniebee <dennie@tellick.nl>
2020-12-02 17:53:30 +0100

    Check correct and valid counts

--- 2020/day02.c
+++ 2020/day02.c
@@ -3,48 +3,44 @@
 #include <string.h>
 
 struct Password {
-    int policy_min;
-    int policy_max;
+    int policy_a;
+    int policy_b;
     char policy_char;
     char password[50];
     int password_length;
-};
+} password_t;
 
 enum TokenType {
-    tt_min = 0,
-    tt_max = 1,
+    tt_a = 0,
+    tt_b = 1,
     tt_char = 2,
     tt_password = 3
-};
-
-struct ListItem {
-    struct Password* data;
-    struct ListItem* next;
 };
 
 int main() {
     char ch;
     FILE* file = fopen("day02_input", "r");
-    struct Password password;
-    enum TokenType token_type = tt_min;
+    struct Password* p;
+    enum TokenType token_type = tt_a;
     char token[50];
-    int16_t token_index = tt_min;
+    int16_t token_index = tt_a;
+    int16_t correct_count = 0;
+    int16_t valid_count = 0;
     while((ch = fgetc(file)) != EOF) {
         switch (token_type)
         {
-        case tt_min:
+        case tt_a:
             if (ch == 45) {
-                password.policy_min = atoi(token);
-                printf("%i\n", password.policy_min);
-                token_type = tt_max;
+                p = malloc(sizeof(password_t));
+                p->policy_a = atoi(token);
+                token_type = tt_b;
                 token_index = 0;
                 continue;
             }
             break;
-        case tt_max:
+        case tt_b:
             if (ch == 32) {
-                password.policy_max = atoi(token);
-                printf("%i\n", password.policy_max);
+                p->policy_b = atoi(token);
                 token_type = tt_char;
                 token_index = 0;
                 continue;
@@ -52,8 +48,7 @@ int main() {
             break;
         case tt_char:
             if (ch == 58) {
-                password.policy_char = token[0];
-                printf("%c\n", password.policy_char);
+                p->policy_char = token[0];
                 token_type = tt_password;
                 token_index = 0;
                 continue;
@@ -62,15 +57,29 @@ int main() {
         case tt_password:
             if (ch == 32) continue;
             if (ch == 10) {
-                strcpy(password.password, token);
-                password.password_length = token_index;
-                for (size_t i = 0; i < token_index; i++) {
-                    printf("%c", password.password[i]);
+                strcpy(p->password, token);
+                p->password_length = token_index;
+
+
+                // part 1
+                int16_t char_count = 0;
+                for (size_t i = 0; i < p->password_length; i++) {
+                    if (p->password[i] == p->policy_char) {
+                        char_count++;
+                    }
                 }
-                printf("\n");
-                token_type = tt_min;
+                if (char_count >= p->policy_a && char_count <= p->policy_b) {
+                    correct_count++;
+                }
+
+                // part 2
+                if (p->password[p->policy_a - 1] == p->policy_char ^ p->password[p->policy_b - 1] == p->policy_char) {
+                    valid_count++;
+                }
+
+                token_type = tt_a;
                 token_index = 0;
-                password.password_length = 0;
+                free(p);
                 continue;
             }
             break;
@@ -79,10 +88,10 @@ int main() {
             break;
         }
 
-        token[token_index++] = ch;
-
-        //printf("%i, %i, %c, %s", password.policy_min, password.policy_max, password.policy_char, password.password);
+        token[token_index++] = ch;   
     }
-
+    fclose(file);
+    printf("Part 1 | Number of correct passwords: %i\n", correct_count);
+    printf("Part 2 | Number of valid passwords: %i\n", valid_count);
     return 0;
 }
