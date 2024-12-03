69b30cea92b1e5ee8497c95d9429ebf672ff1bc9
Dennie Bastiaan <Dennie.Bastiaan@level-up.nl>
2024-12-03 10:57:21 +0100

    Zig convention?

--- 2024/day01.zig
+++ 2024/day01.zig
@@ -1,21 +1,21 @@
 const std = @import("std");
 
-pub fn read_contents(file_name: []const u8) ![]u8 {
+pub fn readContents(file_name: []const u8) ![]u8 {
     const file = try std.fs.cwd().openFile(file_name, .{});
     defer file.close();
 
-    var buf_reader = std.io.bufferedReader(file.reader());
-    const in_stream = buf_reader.reader();
+    var buffered_reader = std.io.bufferedReader(file.reader());
+    const reader = buffered_reader.reader();
     const file_size = (try file.stat()).size;
 
     const buffer = try std.heap.page_allocator.alloc(u8, file_size);
-    try in_stream.readNoEof(buffer);
+    try reader.readNoEof(buffer);
 
     return buffer;
 }
 
 pub fn main() !void {
     const file_name: []const u8 = "day01_input";
-    const buffer = try read_contents(file_name);
+    const buffer = try readContents(file_name);
     std.debug.print("{s}", .{buffer});
 }
