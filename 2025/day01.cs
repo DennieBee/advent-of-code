8eab23112922878603311716e78755dcaee23402
Dennie Bastiaan <dennie@tellick.nl>
2025-12-04 14:59:42 +0100

    Part 2 solution

--- 2025/day01.cs
+++ 2025/day01.cs
@@ -1,21 +1,23 @@
+var part2 = true;
 var cursor = 50;
 var zeros = 0;
 var input = File.OpenText("day01.input").ReadToEnd();
 var lines = input.Split("\n");
 
 foreach (var line in lines)
-{   
+{
     var amount = Convert.ToInt32(line.Substring(1));
-    
-    if (amount > 100) amount -= amount - amount % 100;
+    var inc = 1;
+    if (line.StartsWith("L")) inc = -1;
 
-    if (line.StartsWith("L")) cursor -= amount;
-    else cursor +=amount;
-
-    if (cursor < 0) cursor += 100;
-    if (cursor > 99) cursor -= 100;
-    
-    if (cursor == 0) zeros++;
+    for (int i = 0; i < amount; i++)
+    {
+        cursor += inc;
+        if (cursor == -1) cursor = 99;
+        if (cursor == 100) cursor = 0;
+        if (part2) if (cursor == 0) zeros++;
+    }
+    if (!part2) if (cursor == 0) zeros++;
 }
 
 Console.WriteLine(zeros);
